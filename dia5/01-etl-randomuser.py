8b503eecab9d89452d4cfec980f2b702fb5c3d8c
svianal <samuel.viana2702@gmail.com>
2025-10-24 21:34:55 -0500

    ETL basico

---
+++ dia5/01-etl-randomuser.py
@@ -0,0 +1,69 @@
+import requests
+import mysql.connector
+from datetime import datetime
+from tabulate import tabulate
+from prefect import flow,task
+
+def extract():
+    """
+    extrae data de randomuser
+    """
+    URL = "https://randomuser.me/api/?results=10"
+    response = requests.get(URL)
+    data = []
+    if response.status_code == 200:
+        data = response.json()['results']
+        
+    return data
+
+def transform(data_users):
+    """
+    transformamos la data de randomuser
+    """
+    data_transformed = []
+    for user in data_users:
+        nombre = f"{user['name']['first']} {user['name']['last']}"
+        sexo = user['gender']
+        pais = user['location']['country']
+        fecha_nac = user['dob']['date']
+        fecha_nac = datetime.fromisoformat(fecha_nac.rstrip("Z")).date()
+        data_transformed.append((nombre,sexo,pais,fecha_nac))
+        
+    return data_transformed
+
+def load(data_to_load):
+    resultado = 0
+    conn = mysql.connector.connect(
+       host="localhost",
+       user="root",
+       password="root",
+       database="db_g5"
+    )
+    cursor = conn.cursor()
+    cursor.execute("""
+               CREATE TABLE IF NOT EXISTS random_users (
+               id INT AUTO_INCREMENT PRIMARY KEY,
+               nombre VARCHAR(255),
+               sexo VARCHAR(10),
+               pais VARCHAR(100),
+               fecha_nac DATE)
+               """)
+    conn.commit()
+    insert_query = "INSERT INTO  random_users(nombre,sexo,pais,fecha_nac) VALUES(%s,%s,%s,%s)"
+    cursor.executemany(insert_query,data_to_load)
+    conn.commit()
+    resultado = cursor.rowcount
+    cursor.close()
+    conn.close()
+    return resultado
+
+def main():
+    data = extract()
+    data_transformed = transform(data)
+    cabeceras = ['nombre','sexo','pais','fecha_nac']
+    print(tabulate(data_transformed,headers=cabeceras,tablefmt='grid'))
+    resultado = load(data_transformed)
+    print(f'se insertaron {resultado} registros en la bd')
+
+if __name__ == "__main__":
+    main()
--- dia5/README.md
+++ dia5/README.md
@@ -4,3 +4,5 @@ PASO 2 - ACTIVAR ENTORNO VIRTUAL
 source venv/Scripts/activate
 PASO 3 - INSTALAR DEPENDENCIAS
 pip install -r requirements.txt
+PASO 4 - VER LAS LIBRERIAS
+pip freeze
